d18f89ac70c4622510bbe69c759acb5066ed01b3
tapanmodh <modhtapan@gmail.com>
2026-02-24 16:30:27 +0530

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/tm/thinknote/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.tm.thinknote
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
